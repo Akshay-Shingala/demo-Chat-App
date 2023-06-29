11e60b39772d0abfc72277addf4d1c7315375d80
akki151 <teamtest12757@gmail.com>
2023-06-29 12:06:53 +0530

    list users and chat list get

--- demoChatApp/chatApp/admin.py
+++ demoChatApp/chatApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PersonalChat,GroupMessages,PersonalChatMessage
 
-# Register your models here.
+class PersonalChatMessageAdmin(admin.ModelAdmin):
+    list_display=['personalchat','sender','resiver','message','timestamp']
+admin.site.register(PersonalChatMessage,PersonalChatMessageAdmin)
+
+class PersonalChatAdmin(admin.ModelAdmin):
+    list_display=['id','sender','resiver']
+admin.site.register(PersonalChat,PersonalChatAdmin)
--- demoChatApp/demoChatApp/routs.py
+++ demoChatApp/demoChatApp/routs.py
@@ -1,8 +1,9 @@
 from chatApp.views import UserConsumer
 from django.urls import path
+from .consumers import MyPersonalChatWebsocketConsumer
 # from .consumers import MyGroupChatWebsocketConsumer,MyPersonalChatWebsocketConsumer
 WebSocketUrls=[
-    # path('Personal/<str:username>',MyPersonalChatWebsocketConsumer.as_asgi()),
+    path('Personal/<str:username>',MyPersonalChatWebsocketConsumer.as_asgi()),
     # path('Group/<str:username>',MyGroupChatWebsocketConsumer.as_asgi())
     path('usersLists/',UserConsumer.as_asgi(),name="Ragister")
 ]
