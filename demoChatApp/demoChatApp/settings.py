e9e6fbf67f707faf014b38b1ff46bc35419b4f84
akki151 <teamtest12757@gmail.com>
2023-06-30 12:11:28 +0530

    chat complite
    Signed-off-by: akki151 <teamtest12757@gmail.com>

--- demoChatApp/demoChatApp/settings.py
+++ demoChatApp/demoChatApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
+    # 'daphne',
     'channels',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'demoChatApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'chatApp/templets')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
